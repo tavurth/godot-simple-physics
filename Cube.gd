5ff62e4a033ae6faaa7fea3d01ac139562486bb7
will <tavurth@gmail.com>
2019-05-14 16:10:35 +0700

    Apply local impulse

--- Cube.gd
+++ Cube.gd
@@ -5,14 +5,16 @@ const ACCELERATION = 0.5
 func _ready():
 	set_process_input(true)
 
+func apply_local_impulse(impulseDirection):
+	var global_direction = global_transform.basis.xform(impulseDirection.normalized())
+
+	apply_impulse(Vector3(0,0,0), global_direction * impulseDirection) # Apply to bodies center
+
 func _input(event):
 	## Do nothing if we haven't got the space key pressed
 	## See Project -> Project Settings -> Input map
 	if not Input.is_action_pressed('action_impulse'):
 		return
 
+	apply_local_impulse(Vector3(0, -5, 0))
 
-	self.apply_impulse(
-		self.get_translation() - Vector3(0, 10, 0),
-		Vector3(0, ACCELERATION, ACCELERATION / 10)
-	)
