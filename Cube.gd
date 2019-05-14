1c015077899853ed3bb5570375750a2ba5c57a16
will <tavurth@gmail.com>
2019-05-14 15:59:36 +0700

    Fix bugs with events

--- Cube.gd
+++ Cube.gd
@@ -1,16 +1,18 @@
 extends RigidBody
 
+const ACCELERATION = 0.5
+
 func _ready():
-	set_process(true)
-	
-func _process(delta):
+	set_process_input(true)
+
+func _input(event):
 	## Do nothing if we haven't got the space key pressed
 	## See Project -> Project Settings -> Input map
 	if not Input.is_action_pressed('action_impulse'):
 		return
-	
-	print("Applying impulse")
+
+
 	self.apply_impulse(
-		self.get_translation(),
-		Vector3(0, 100, 0)
+		self.get_translation() - Vector3(0, 10, 0),
+		Vector3(0, ACCELERATION, ACCELERATION / 10)
 	)
